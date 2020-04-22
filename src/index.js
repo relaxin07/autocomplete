b6569d51b09cb14e11a4bb13239b8f2828b1a209
relax1n <relaxinfh@mail.ru>
2020-04-22 23:43:54 +0300

    add provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import Autocomplete from '././autocomplete/Autocomplete'
+import { Provider } from 'react-redux'
+import store from './store/store'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <Autocomplete />
+  </Provider>,
+  document.getElementById('root')
+)
