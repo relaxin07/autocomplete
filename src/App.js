4c4588b281b6d47b5b44e3ebc45729609a0d7c76
relax1n <relaxinfh@mail.ru>
2020-04-22 23:45:18 +0300

    change app component for func component

--- src/App.js
+++ src/App.js
@@ -1,127 +1,32 @@
 import React from 'react'
-import { countries } from './country'
 import './App.scss'
 
-class App extends React.Component {
-  allCountries = countries
-  state = {
-    inputValue: '',
-    data: null,
-    indexActiveItem: -1,
+const renderData = (arr, indexActive) => {
+  if (arr === null) {
+    return false
   }
-
-  onChange = (e) => {
-    this.setState({ inputValue: e.target.value })
-    this.dataHandler(e.target.value.toLowerCase())
-  }
-
-  onKeyDown = (e) => {
-    let { indexActiveItem } = this.state
-    switch (e.keyCode) {
-      case 38:
-        indexActiveItem--
-        this.defineIndexActive(indexActiveItem)
-        break
-      case 40:
-        indexActiveItem++
-        this.defineIndexActive(indexActiveItem)
-        break
-      case 13:
-        this.choiceSelect(indexActiveItem)
-        break
-    }
-  }
-  //Получаем индекс выбранного элемента.
-  choiceSelect = (indexCurrent) => {
-    const { data } = this.state
-    if (data !== null) {
-      this.setState(() => ({
-        inputValue: data[indexCurrent],
-        data: null,
-        indexActiveItem: -1,
-      }))
-    }
-  }
-  // фильруем данные по символам с инпута.
-  dataHandler = (value) => {
-    let arrdata = []
-    if (value.length > 1) {
-      arrdata = this.allCountries.filter((country, i) => {
-        return country.toLocaleLowerCase().includes(value)
-      })
-
-      this.setState(() => ({
-        data: arrdata,
-      }))
-    } else {
-      this.setState({ data: null })
-    }
-  }
-  //получаем индекс активного элемента
-  defineIndexActive = (indexActiveItem) => {
-    const { data } = this.state
-    if (data === null) {
-      return false
-    }
-    if (indexActiveItem === data.length) {
-      indexActiveItem = 0
-    }
-    if (indexActiveItem < 0) {
-      indexActiveItem = data.length - 1
-    }
-    this.setState(() => ({
-      indexActiveItem,
-    }))
-  }
-
-  render() {
-    const { data, indexActiveItem, inputValue } = this.state
-    let resultData = []
-    if (data !== null) {
-      // готовим массив данных к выводу
-      resultData = data.map((item, i) => {
-        if (indexActiveItem !== null && indexActiveItem === i) {
-          return (
-            <div key={i} className="hint-item active">
-              {item}
-            </div>
-          )
-        }
-        return (
-          <div
-            key={i}
-            className="hint-item"
-            onClick={() => {
-              this.choiceSelect(i)
-            }}
-          >
-            {item}
-          </div>
-        )
-      })
-    }
-
-    return (
-      <div className="container">
-        <h2>SelectBox</h2>
-        <h5>Search countries</h5>
-        <input
-          value={inputValue}
-          className="main-input"
-          type="text"
-          onKeyDown={(event) => {
-            this.onKeyDown(event)
-          }}
-          onChange={(event) => this.onChange(event)}
-        />
-        <div>
-          {resultData.map((items) => {
-            return items
-          })}
+  return arr.map((item, i) => {
+    for (const val in item) {
+      let activeClass = i === indexActive ? 'active' : ''
+      return (
+        <div key={i} className={`hint__item ${activeClass}`}>
+          {item[val]}
         </div>
-      </div>
-    )
-  }
+      )
+    }
+  })
+}
+
+function App(props) {
+  const { indexActive, input } = props
+  let resultRender = renderData(props.prepareData(props.data), indexActive)
+  return (
+    <div className="container">
+      <h2>Selectbox</h2>
+      {input}
+      <div>{resultRender}</div>
+    </div>
+  )
 }
 
 export default App
